10250fec9e280b2b15e1efc51a2b651c25d99b2b
Hugh <shavenking@gmail.com>
2017-04-21 17:28:08 +0800

    Inital

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', function () { echo 'hello world'; });
+});
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+if (FastRoute\Dispatcher::FOUND !== $routeInfo[0]) {
+    // gg886
+}
+
+$handler = $routeInfo[1];
+$params = $routeInfo[2];
+
+$handler(...$params);
